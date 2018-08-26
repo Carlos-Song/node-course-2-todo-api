c73052ecfdbf246f094ac5c299931072b38376d3
silverwing <silverwing@silverwingdeiMac.local>
2018-08-27 03:21:19 +0800

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,15 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    // 把发送的消息包裹成一个对象的属性，使其可以添加一些自定义的提示
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
